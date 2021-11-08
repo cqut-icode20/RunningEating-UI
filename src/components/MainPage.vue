b8683047508bced99135cc286dcc89d0813637a7
LIXIN <763819849@qq.com>
2021-11-08 13:00:32 +0800

    修改界面，引入echarts

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,13 +1,79 @@
 <template>
-<div><h1>MainPageHere</h1></div>
+  <div class="report">
+    <!--  卡片-->
+    <el-card>
+      <!-- 2. 为ECharts准备一个具备大小（宽高）的Dom -->
+      <div class="chart" id="sales" ></div>
+    </el-card>
+    <el-card>
+      <div class="chart" id="users"></div>
+    </el-card>
+    <el-card>
+      <div class="chart" id="login"></div>
+    </el-card>
+    <el-card>
+      <div class="chart" id="fire"></div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+
+import * as echarts from "echarts";
+
 export default {
-  name: "MainPage"
+  name: 'MainPage',
+  components: {},
+  async mounted() {
+    const chart_sales = echarts.init(document.getElementById('sales'));
+    const chart_users = echarts.init(document.getElementById('users'));
+    const chart_login = echarts.init(document.getElementById('login'));
+    const chart_fire = echarts.init(document.getElementById('fire'));
+// 指定图表的配置项和数据
+    const option = {
+      title: {
+        x: 'center',
+        y: 'top',
+        text: '实时销量数据'
+      },
+      tooltip: {},
+      legend: {
+        y: 'bottom',
+        data: ['销量']
+      },
+      xAxis: {
+        data: ["零食1", "零食2", "零食3", "零食4", "零食5", "零食6"]
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [35, 20, 36, 10, 10, 20]
+      }]
+    };
+
+// 使用刚指定的配置项和数据显示图表。
+    chart_sales.setOption(option);
+    chart_users.setOption(option);
+    chart_login.setOption(option);
+    chart_fire.setOption(option);
+  }
 }
 </script>
 
 <style scoped>
+.report{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  align-items: center;
+
+}
+.chart{
+  width: 400px;
+  height: 300px;
+}
 
 </style>
--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,86 +1,86 @@
 <template>
   <div class="home">
     <el-container class="home-container">
-      <!--    头部区域-->
-      <el-header>
-        <div>
-          <img src="../../assets/logo.png" alt=""/>
-          <h3>跑着吃，后台管理系统</h3>
-        </div>
-        <div>
-          <el-menu
-              class="el-menu-top"
-              mode="horizontal"
-              @select="handleSelect"
-              background-color="#373d3f"
-              text-color="#fff"
-              active-text-color="#ffd04b">
-            <el-submenu :index="1+'t'">
-              <template slot="title">
-                <i class="el-icon-user"></i>
-                <span>{{ "Hello," + user.username }}</span>
-              </template>
-              <el-menu-item index="1-1">
-                <template slot="title">
-                  <i class="el-icon-s-custom"></i>
-                  <span>个人中心</span>
-                </template>
-              </el-menu-item>
-              <el-menu-item index="2-2">
-                <template slot="title">
-                  <i class="el-icon-s-release"></i>
-                  <span>退出登录</span>
-                </template>
-              </el-menu-item>
-            </el-submenu>
-            <el-menu-item index="2'">
-              <template slot="title">
-                <i class="el-icon-chat-dot-square"></i>
-                <span>消息</span>
-              </template>
+      <!--    侧边栏区域-->
+      <el-aside :width="isCollapse ?'64px':'200px'">
+        <el-menu
+            unique-opened="true"
+            :collapse-transition="false"
+            :collapse="isCollapse"
+            default-active="1_id"
+            class="el-menu-vertical-aside"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#373d3f"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+
+          <!--            一级标题-->
+          <el-submenu :index="item.id+'_id'" v-for="item in menuList" :key="item.id" @click="handleOpen(item.path)">
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{ item.title }}</span>
+            </template>
+            <el-menu-item :index="i.id+'_id'" v-for="i in item.children" :key="i.id" @click="handleOpen(i.path)">
+              {{ i.title }}
             </el-menu-item>
-          </el-menu>
-        </div>
-      </el-header>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <!--    中部区域-->
       <el-container class="home-container-mid">
-        <!--  侧边栏-->
-        <el-aside>
-
-          <el-menu
-              :collapse-transition="true"
-              :collapse="isCollapse"
-              default-active="1_id"
-              class="el-menu-vertical-aside"
-              @open="handleOpen"
-              @close="handleClose"
-              background-color="#373d3f"
-              text-color="#fff"
-              active-text-color="#ffd04b">
-            <el-menu-item index="1">
-              <template slot="title">
-                <i style="color: #5dbe8a" class="el-icon-s-fold" @click="collapse()"></i>
-                <span style="color: #5dbe8a" @click="collapse()">{{ isCollapse ? "展开" : "折叠" }}</span>
-              </template>
-            </el-menu-item>
-            <!--            一级标题-->
-            <el-menu-item index="2" @click="handleOpen(mainPage.path)">
-              <template slot="title">
-                <i :class="iconsObj[0]"></i>
-                <span>{{ mainPage.title }}</span>
-              </template>
-            </el-menu-item>
-            <el-submenu :index="item.id+'_id'" v-for="item in menuList" :key="item.id" @click="handleOpen(item.path)">
-              <template slot="title">
-                <i :class="iconsObj[item.id]"></i>
-                <span>{{ item.title }}</span>
-              </template>
-              <el-menu-item :index="i.id+'_id'" v-for="i in item.children" :key="i.id" @click="handleOpen(i.path)">
-                {{ i.title }}
+        <!--  头部-->
+        <el-header>
+          <div>
+            <div slot="title">
+              &nbsp;
+              &nbsp;
+              <i :class="isCollapse?'el-icon-s-unfold':'el-icon-s-fold'" @click="collapse()"></i>
+              <!--              <span style="color: #5dbe8a" @click="collapse()">{{ isCollapse ? "展开" : "折叠" }}</span>-->
+            </div>
+            &nbsp;
+            &nbsp;
+            <img src="../../assets/img.png" alt=""/>
+            &nbsp;
+            <h3>跑着吃，后台管理系统</h3>
+          </div>
+<!--         右侧菜单栏-->
+          <div>
+            <el-menu
+                class="el-menu-top"
+                mode="horizontal"
+                @select="handleSelect"
+                background-color="#373d3f"
+                text-color="#fff"
+                active-text-color="#ffd04b">
+              <el-submenu :index="1+'t'">
+                <template slot="title">
+                  <i class="el-icon-user"></i>
+                  <span>{{ "Hello," + user.username }}</span>
+                </template>
+                <el-menu-item index="personalCenter">
+                  <template slot="title">
+                    <i class="el-icon-s-custom"></i>
+                    <span>个人中心</span>
+                  </template>
+                </el-menu-item>
+                <el-menu-item index="2-2" @click="logout()">
+                  <template slot="title">
+                    <i class="el-icon-s-release"></i>
+                    <span>退出登录</span>
+                  </template>
+                </el-menu-item>
+              </el-submenu>
+              <el-menu-item index="2'">
+                <template slot="title">
+                  <i class="el-icon-chat-dot-square"></i>
+                  <span>消息</span>
+                </template>
               </el-menu-item>
-            </el-submenu>
-          </el-menu>
-        </el-aside>
+            </el-menu>
+          </div>
+        </el-header>
+
         <el-main>
           <router-view>
 
@@ -99,12 +99,14 @@ export default {
   data() {
     return {
       isCollapse: false,//时候启用侧边栏折叠
+      //用户信息
       user: {
         username: "智商低"
       },
-      mainPage: {title: '首页统计', id: 0, path: '/home/mainPage'},
+      //菜单列表
       menuList: [
         //一级标题                  二级标题
+        {title: '首页统计', id: 0, path: '/none', children: [{title: '数据统计', id: 12, path: '/home/mainPage'}]},
         {title: '用户管理', id: 1, path: '/none', children: [{title: '用户列表', id: 6, path: '/home/userList'}]},
         {
           title: '权限管理',
@@ -133,24 +135,26 @@ export default {
   },
   methods: {
     //退出登录
-
+    logout() {
+      logger.info("退出登录:" + this.user.username)
+    },
     //打开菜单时
     handleOpen(path) {
       this.isCollapse = false;
-      logger.info('handleOpen:path=' + path)
+      logger.info('打开菜单栏:path=' + path)
       // 跳转到指定页面
-      if (path.match('/') && this.$route.path != path)
+      if (path.match('/') && this.$route.path !== path)
         this.$router.push(path)
     },
     // 关闭菜单时
     handleClose() {
     },
     handleSelect(key) {
-      logger.info('handleOpen:path=' + key)
+      logger.info('打开菜单栏:path=' + key)
 
     },
     collapse() {
-      logger.info('collapse Called')
+      logger.info('菜单栏折叠:'+!this.isCollapse)
       this.isCollapse = !this.isCollapse;
     }
   }
@@ -185,6 +189,7 @@ export default {
 }
 
 .el-header {
+  width: 100% !important;
   display: flex;
   margin: 0;
   justify-content: space-between;
