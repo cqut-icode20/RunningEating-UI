e8b0fe519f113bc35d0c3240ca7fd09c035128b4
userlovecoding <3389274991@qq.com>
2021-11-12 21:21:36 +0800

    userList基本完成

--- src/components/order/HistoryOrderList.vue
+++ src/components/order/HistoryOrderList.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <h1>HistoryListHere</h1>
+  <div class="order">
+    <Breadcrumb name1="订单管理" name2="订单列表" />
+    <el-card>
+      <!--搜索-->
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" clearable v-model="queryInfo.query" @clear="getOderList">
+
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 
 </template>
